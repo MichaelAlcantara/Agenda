3d56bab9d8c8a372d8680ca1fed6f4734e59e8d4
MichaelAlcantara <michael.raposo@hotmail.com>
2023-08-20 16:17:45 -0300

    Formulario de contatos

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +19,10 @@ import { ContatoService } from './contato.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [
     ContatoService
