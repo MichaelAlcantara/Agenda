48265d7e29e302f3c0bfbd14bdc4d02af17b2b93
MichaelAlcantara <michael.raposo@hotmail.com>
2023-08-20 16:56:06 -0300

    Trabalhando com Reactive Forms

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
@@ -24,7 +25,8 @@ import { ContatoService } from './contato.service';
     MatToolbarModule,
     MatInputModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     ContatoService
--- agenda-app/src/app/contato/contato.component.ts
+++ agenda-app/src/app/contato/contato.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Contato } from './contato';
 import { ContatoService } from '../contato.service';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-contato',
@@ -9,10 +11,25 @@ import { ContatoService } from '../contato.service';
 })
 export class ContatoComponent implements OnInit {
 
+  formulario: FormGroup;
+
   constructor(
-    private service: ContatoService
+    private service: ContatoService,
+    private fb: FormBuilder
   ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { 
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.email]
+    })
+  }
+
+  submit(){
+    console.log(this.formulario.value)
+    //this.service.save(c).subscribe( reposta => {
+    //  console.log();
+    //})
+  }
 
 }
