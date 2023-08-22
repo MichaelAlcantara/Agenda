365ffb4eba4ed0eaa833b9f6817513ee477fe0d2
MichaelAlcantara <michael.raposo@hotmail.com>
2023-08-21 22:57:49 -0300

    Mostrando a foto do contato

--- agenda-app/src/app/contato.service.ts
+++ agenda-app/src/app/contato.service.ts
@@ -28,6 +28,6 @@ export class ContatoService {
   }
 
   upload(contato : Contato, formData : FormData) : Observable<any>{
-    return this.http.put(`${this.url}/${contato.id}/foto`, formData);
+    return this.http.put(`${this.url}/${contato.id}/foto`, formData, { responseType : 'blob'});
   }
 }
